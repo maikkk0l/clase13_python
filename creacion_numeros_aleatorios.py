5a5db52e6c47c7ccb8f65b56015a31b99172fadb
Maikol Pino <maik.pino@duocuc.cl>
2025-06-06 11:04:06 -0400

    version 3
    nose k poner

--- creacion_numeros_aleatorios.py
+++ creacion_numeros_aleatorios.py
@@ -6,4 +6,11 @@ for i in range(100): #ciclo para repartir 100 veces las interacciones
     n = random.randint(1,100) #crea el numero entre 1 y 100
     numeros.append(n) #guardamos en la lista al numero aleatorio
 
-print(numeros)
+print(numeros) 
+
+#contadore de numeros pares
+cont = 0
+for i in range(len(numeros)):
+    if numeros[i]%2==0:
+        cont+=1
+print(f"canttidad de numeros pares : {cont}")
